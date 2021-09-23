04d5063dafa1cc6652069f6dd786f2b2ce2a618c
magnoweege <magnoo@gmail.com>
2021-09-23 18:26:02 -0300

    Criado as configurações do swagger ui, acessando via localhost.

---
+++ config/CORSConfig.java
@@ -0,0 +1,31 @@
+package com.tcsloja.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CORSConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(false)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry  registry) {
+        registry.addResourceHandler("**/**")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+
+        registry.
+                addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
---
+++ config/SwaggerConfig.java
@@ -0,0 +1,47 @@
+package com.tcsloja.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    /*
+     * @return A builder which is intended to be the primary interface into the Springfox framework.
+     * Provides sensible defaults and convenience methods for configuration.
+     */
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+
+    /*
+             @return API
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("TCS Loja")
+                .description("Teste TCS")
+                .version("1.0.0")
+                .license("--")
+                .licenseUrl("--")
+                .contact(new Contact("Magno", "", "magnoo@gmail.com"))
+                .build();
+    }
+}
+
